e103997b71fa5b36844ac403f4e047a0c54384a5
AsistDoctor <rebyssine@gmail.com>
2026-07-18 18:38:49 +0300

    say hello but its child

--- Mao/Paketic/main.java
+++ Mao/Paketic/main.java
@@ -5,6 +5,9 @@ public class main {
     public static void  main(String[] args) {
         classnuyPakitic hello = new classnuyPakitic();
         System.out.println(hello.sayHello());
+
+        childPaketic secondHello = new childPaketic();
+        System.out.println(secondHello.sayHello());
     }
 
 }
